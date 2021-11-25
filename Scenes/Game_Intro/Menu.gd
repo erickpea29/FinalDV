bf9eb45537eb868dd0719615c64eafacfb83b232
Fabian_Rmz <mario.354a@hotmail.com>
2021-11-25 15:12:11 -0600

    Modificaciones en menu, importacion de audio y fuentes

--- Scenes/Game_Intro/Menu.gd
+++ Scenes/Game_Intro/Menu.gd
@@ -1,6 +1,24 @@
 extends Node2D
 
+var time_begin
+var time_delay
 
+
+func _ready():
+	$BackgroundAudio.play()
 
 func _on_Button_pressed():
 	get_tree().change_scene("res://Scenes/Top_Level/Room_Scene.tscn")
+
+
+func _on_Exit_pressed():
+	$".".get_tree().quit()
+
+
+func _on_Instructions_pressed():
+	pass # Replace with function body.
+	#get_tree().change_scene("res://Scenes/Top_Level/Room_Scene.tscn")
+
+
+func _on_BackgroundAudio_finished():
+	$BackgroundAudio.play()
