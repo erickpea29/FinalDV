ba8912e23f3526a2344e0d8fb61b7ca707103e5a
Fabian_Rmz <mario.354a@hotmail.com>
2021-11-26 14:35:30 -0600

    Simplificando codigo de disparo

--- Scripts/Gun_Bullet.gd
+++ Scripts/Gun_Bullet.gd
@@ -1,20 +1,20 @@
 extends KinematicBody2D
 
 #Declaracion de variables
-var velocidad = 250 #Velocidad de la bala
+var velocidad = 350 #Velocidad de la bala
 var movimiento = Vector2() #Vector de movimiento de la bala
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-func inicio(posicion_inicial, movimiento_vista):
-	if movimiento_vista:
-		position = -posicion_inicial
-		movimiento = Vector2(-velocidad, 0)
-	else:
-		position = posicion_inicial
-		movimiento = Vector2(velocidad, 0)
+func inicio(posicion_inicial):
+	#if movimiento_vista:
+	position = posicion_inicial
+		#movimiento = Vector2(-velocidad, 0)
+	#else:
+		#position = posicion_inicial
+	movimiento = Vector2(velocidad, 0)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
--- Scripts/Main_Character_Animation.gd
+++ Scripts/Main_Character_Animation.gd
@@ -3,34 +3,34 @@ extends KinematicBody2D
 var velocidad = 250 #Velocidad del personaje
 var movimiento = Vector2() #Vector de movimiento de la velocidad
 var con_slide = true #Variable para cambiar entre move_and_slide() y move_and_collide()
-var arma = false
+var arma = false #Variable que nos permite poner y quitar sprite de personaje con arma
+#Cargamos escena de vala para instanciarla
 var dispara_bala = preload("res://Scenes/Objects/Gun_Bullet.tscn")
-var vista
 
 func _ready():
 	self.global_position = Global.player_initial_map_position
 
+#Funcion para escuchar eventos del teclado
 func get_input():
-	movimiento = Vector2()
+	movimiento = Vector2() #Asignamos vector de movimiento
 	if Input.is_action_pressed('ui_right'):
-		$Player.flip_h = false #Si el personaje gira a la derecha
-		vista = false
-		movimiento.x += 1
+			$Player.flip_h = false #Si el personaje gira a la derecha
+			movimiento.x += 1
 	if Input.is_action_pressed('ui_left'):
-		$Player.flip_h = true #Si el personaje gira a la izquierda
-		$Player/Gun_Sight.transform.x * -1
-		vista = true
-		movimiento.x -= 1
-	if Input.is_action_pressed('ui_down'):
-		movimiento.y += 1
-	if Input.is_action_pressed('ui_up'):
-		movimiento.y -= 1
-	
+			$Player.flip_h = true #Si el personaje gira a la izquierda
+			movimiento.x -= 1
+	if Input.is_action_pressed('ui_down'): #Si el personaje va hacia abajo
+			movimiento.y += 1
+	if Input.is_action_pressed('ui_up'): #Si el personaje va hacia arriba
+			movimiento.y -= 1
+
+	#Normalizamos velocidad para desplazamiento en diagonal
 	movimiento = movimiento.normalized() * velocidad
-	
+
+	#Verificamos si el jugador porta o guarda el arma, cuando la consiga
 	if Input.is_action_just_pressed("TakeGun"):
 		arma = !arma
-	
+
 	#Verificamos que el personaje este en movimiento
 	if movimiento.x != 0 || movimiento.y != 0:
 		$Player.playing = true #Iniciamos las animaciones
@@ -41,19 +41,22 @@ func get_input():
 	else: #Si no está en movimiento verificamos que tenga o no arma
 		if arma:
 			$Player.animation = "Main_Character_Shooting" #Sprite del personaje cuando está quieto y tiene arma
+			#Verificamos que tenga arma y que se haya presionado F (disparo)
+			if Input.is_action_just_pressed("Shoot"):
+				$Player.animation = "Main_Character_Shooting"
+				print("hola hola")
+				$Shoot_SoundEffect.play()
+				shoot() #Funcion que genera la bala y realiza el disparo
 		else:
 			$Player.animation = "Main_Character_Idle" #Sprite del personaje cuando está quieto y no tiene arma
+			$Shoot_SoundEffect.volume_db = -80
 		$Steps_SoundEffect.pitch_scale = rand_range(0.8, 1.2) #Modulacion del sonido de los efectos de pasos del personaje
 		$Steps_SoundEffect.play() #Efecto de sonido de pasos del personaje
-	#Verificamos que tenga arma y que se haya presionado F (disparo)
-	
-	if arma && Input.is_action_just_pressed("Shoot"):
-		shoot() #Funcion que genera la bala y realiza el disparo
 
 #Funcion para generar la bala y animar el disparo del arma del personaje
 func shoot():
 	var bala = dispara_bala.instance() #Instanciamos la bala
-	bala.inicio($Player/Gun_Sight.global_position, vista) #Iniciamos valores de posicion para generar el disparo
+	bala.inicio($Player/Gun_Sight.global_position) #Iniciamos valores de posicion para generar el disparo
 	get_parent().add_child(bala) #Agregamos la escena de la bala
 
 func _physics_process(delta):
