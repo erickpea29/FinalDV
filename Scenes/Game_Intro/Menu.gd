1ba3ff00c6bc2d5e1f7faf68f049096455530c4d
Fabian_Rmz <mario.354a@hotmail.com>
2021-11-25 19:26:29 -0600

    Agregando particulas

--- Scenes/Game_Intro/Menu.gd
+++ Scenes/Game_Intro/Menu.gd
@@ -1,7 +1,10 @@
 extends Node2D
 
+#VARIABLES
+
 func _ready():
 	$BackgroundAudio.play()
+	$Mouse_Pointer.emitting = true
 
 func _on_Button_pressed():
 	get_tree().change_scene("res://Scenes/Top_Level/Room_Scene.tscn")
@@ -15,3 +18,27 @@ func _on_Instructions_pressed():
 
 func _on_BackgroundAudio_finished():
 	$BackgroundAudio.play()
+
+func _input(event):
+   # Mouse in viewport coordinates.
+	if event is InputEventMouseMotion:
+		$Mouse_Pointer.global_position = event.position
+
+func botones():
+	if !$Control/Start.is_hovered():
+		$Control/Start.flat = true
+	else:
+		$Control/Start.flat = false
+	
+	if !$Control/Instructions.is_hovered():
+		$Control/Instructions.flat = true
+	else:
+		$Control/Instructions.flat = false
+	
+	if !$Control/Exit.is_hovered():
+		$Control/Exit.flat = true
+	else:
+		$Control/Exit.flat = false
+
+func _physics_process(delta):
+	botones()
