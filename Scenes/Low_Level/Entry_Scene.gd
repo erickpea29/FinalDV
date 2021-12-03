a682596940e524de4f84e10455a9094f503812b4
Fabian_Rmz <mario.354a@hotmail.com>
2021-12-02 18:52:12 -0600

    Creacion de dialogos en escenas

--- Scenes/Low_Level/Entry_Scene.gd
+++ Scenes/Low_Level/Entry_Scene.gd
@@ -4,6 +4,7 @@ extends Node2D
 const MySmokeResource = preload("res://Assets/enemy/Enemy.tscn")
 
 func _ready():
+	$Dialog.hide()
 	if Global.bul == false: 
 		print("Hola")
 		#Make instance
@@ -11,3 +12,22 @@ func _ready():
 		#Attach it to the tree
 		GrabedInstance.position = Vector2(830,450)
 		self.add_child(GrabedInstance)
+
+#OBJECT INTERACTION
+#Entry Door
+func _on_Object_Int_body_entered(body):
+	$Dialog.text = str("Hmm, seems to be locked, maybe I need a key...")
+	$Dialog.show()
+	$Hide_Dialog.start()
+func _on_Hide_Dialog_timeout():
+	$Dialog.hide()
+#Dekstop
+func _on_Desktop_Int_body_entered(body):
+	$Dialog.text = str("I need to clean up this mess...")
+	$Dialog.show()
+	$Hide_Dialog.start()
+#Door
+func _on_Area2D_body_entered(body):
+	$Dialog.text = str("Locked.")
+	$Dialog.show()
+	$Hide_Dialog.start()
