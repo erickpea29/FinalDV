0e779d620b81226403f77d9de2872f44a86a974c
Erick <erick.penavaz@anahuac.mx>
2021-12-02 20:43:39 -0600

    Agregando sonidos

--- Scenes/Objects/Gun.gd
+++ Scenes/Objects/Gun.gd
@@ -1,9 +1,13 @@
 extends Area2D
 
+const MySmokeResource = preload("res://Scenes/Sounds/Monster_1.tscn")
 
 func _on_Node2D_body_entered(body):
 		Global._prueba()
+		MySmokeResource.instance()
 		self.queue_free()
+
+		
 
 
 
