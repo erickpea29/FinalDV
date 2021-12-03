9796034f3cec4a486dceb1d2cb78ec36f42a77c8
Fabian_Rmz <mario.354a@hotmail.com>
2021-12-02 20:41:22 -0600

    Agregando interaccion de objetos en escenas superiores

--- Scenes/Low_Level/Kitchen_Scene.gd
+++ Scenes/Low_Level/Kitchen_Scene.gd
@@ -2,4 +2,16 @@ extends Node2D
 
 
 func _ready():
-	pass
+	$Dialog.hide()
+
+#INTERACTION WITH OBJECTS
+#Table
+func _on_Table_body_entered(body):
+	$Dialog.text = str("No time for have snack")
+	$Dialog.show()
+	$Show_Dialogs.start()
+#Stove
+func _on_Stove_body_entered(body):
+	$Dialog.text = str("Just a stand of plates")
+	$Dialog.show()
+	$Show_Dialogs.start()
--- Scenes/Top_Level/Room_Scene.gd
+++ Scenes/Top_Level/Room_Scene.gd
@@ -3,6 +3,7 @@ extends Node2D
 const MySmokeResource = preload("res://Scenes/Objects/Key.tscn")
 
 func _ready():
+	$Dialog.hide()
 	if Global.bul == false: 
 		#Make instance
 		var GrabedInstance= MySmokeResource.instance()
@@ -10,3 +11,19 @@ func _ready():
 		GrabedInstance.position = Vector2(660,265)
 		self.add_child(GrabedInstance)
 
+#OBJECT INTERACTION
+#Window
+func _on_Window_body_entered(body):
+	$Dialog.text = str("It´s raining really hard...")
+	$Dialog.show()
+	$Hide_Dialogs.start()
+#Buro
+func _on_Buro_body_entered(body):
+	$Dialog.text = str("I´ve found a key...")
+	$Dialog.show()
+	$Hide_Dialogs.start()
+#Buro2
+func _on_Buro2_body_entered(body):
+	$Dialog.text = str("Just another buro.")
+	$Dialog.show()
+	$Hide_Dialogs.start()
